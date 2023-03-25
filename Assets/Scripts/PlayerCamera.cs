68a0e98f92510aa620671746d3b2ebdad18eda57
Thomas Zhu <silver6219@hotmail.com>
2023-03-25 13:37:05 +1100

    Initial Adv Player movement testing code/scene added

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sens;
+
+    public Transform orientation;
+
+    private float xRot;
+    private float yRot;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // Getting mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sens;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sens;
+
+        yRot += mouseX;
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90, 90);
+
+        // Rotate Camera
+        transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+        orientation.rotation = Quaternion.Euler(0, yRot, 0);
+    }
+}
