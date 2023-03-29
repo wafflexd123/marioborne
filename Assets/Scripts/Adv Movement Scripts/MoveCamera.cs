ac069fced33d85ede90d749d9bb27dab806767a8
wafflexd123 <54018924+wafflexd123@users.noreply.github.com>
2023-03-29 23:12:15 +1100

    Office entrance level

--- Assets/Scripts/Adv Movement Scripts/MoveCamera.cs
+++ Assets/Scripts/Adv Movement Scripts/MoveCamera.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class MoveCamera : MonoBehaviour
 {
     public Transform cameraPosition;
