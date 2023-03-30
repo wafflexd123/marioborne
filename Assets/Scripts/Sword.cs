1d0d4f7ff7d8660a1e1bccafbd0184d4f5033487
Miksi64 <53842268+Miksi64@users.noreply.github.com>
2023-03-30 19:38:13 +1100

    added delay to sword deflect

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,7 +6,9 @@ public class Sword : WeaponBase
 	public Position guardPos;
 	public float guardSpeed;
 	Coroutine crtGuard;
+    Coroutine crtDelay;
 	GameObject reflectWindow;
+    public float reflectDelay;
 
 	protected override void OnPickup()
 	{
@@ -14,33 +16,36 @@ public class Sword : WeaponBase
 		reflectWindow = wielder.transform.Find("ReflectWindow").gameObject;
 	}
 
-	protected override void RightMouse()
-	{
-		if (crtGuard == null) crtGuard = StartCoroutine(Guard());
-		IEnumerator Guard()
-		{
-			while (BeingHeld())//make sure to end coroutine if weapon is dropped
-			{
-				if (wielder.GetAxis("Mouse") > 0)//if right mouse is still being held
-				{
-					if (transform.localPosition != guardPos.coords)
-					{
-						transform.localEulerAngles = guardPos.eulers;//temp
-						transform.localPosition = Vector3.MoveTowards(transform.localPosition, guardPos.coords, pickupSpeed * Time.deltaTime);
-					}
-					reflectWindow.SetActive(true);
-				}
-				else if (transform.localPosition != handPosition.coords)
-				{
-					transform.localEulerAngles = handPosition.eulers;//temp
-					transform.localPosition = Vector3.MoveTowards(transform.localPosition, handPosition.coords, pickupSpeed * Time.deltaTime);
+    protected override void RightMouse()
+    {
+        if (crtDelay == null) crtGuard = StartCoroutine(Guard());
+        IEnumerator Guard()
+        {
+            if (transform.localPosition != guardPos.coords)
+            {
+                transform.localEulerAngles = guardPos.eulers;//temp
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, guardPos.coords, pickupSpeed * Time.deltaTime);
+            }
 
-					reflectWindow.SetActive(false);
-				}
-				else break;//if not pressing mouse and not holstering
-				yield return null;
-			}
-			crtGuard = null;
-		}
-	}
+            reflectWindow.SetActive(true);
+            yield return new WaitForSeconds(0.75f);
+
+            if (transform.localPosition != handPosition.coords)
+            {
+                transform.localEulerAngles = handPosition.eulers;//temp
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, handPosition.coords, pickupSpeed * Time.deltaTime);
+
+                reflectWindow.SetActive(false);
+            }
+            crtGuard = null;
+            crtDelay = StartCoroutine(Delay());
+            yield return null;
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(reflectDelay);
+        crtDelay = null;
+    }
 }
