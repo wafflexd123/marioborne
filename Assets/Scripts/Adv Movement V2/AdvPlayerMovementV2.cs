feac38978e3875b8a23b7ef28a6aea385290da2a
wafflexd123 <54018924+wafflexd123@users.noreply.github.com>
2023-04-06 02:36:24 +1000

    some refactoring of player movement

--- Assets/Scripts/Adv Movement V2/AdvPlayerMovementV2.cs
+++ Assets/Scripts/Adv Movement V2/AdvPlayerMovementV2.cs
@@ -4,200 +4,193 @@ using UnityEngine;
 
 public class AdvPlayerMovementV2 : MonoBehaviour
 {
-    [Header("Movement")]
-    private float moveSpeed;
-    public float walkSpeed = 6f;
-    public float sprintSpeed = 12f;
-    public float movementMultiplier = 10f;
-    public float playerHeight = 2;
-    [SerializeField] float acceleration;
-    [SerializeField] float airMultiplier = 0.8f;
-    [SerializeField] float jumpCooldown = 0.2f;
-    [SerializeField] float dashCooldown = 2f;
-    [SerializeField] Transform orientation;
-    [SerializeField] float jumpForce = 50f;
-    [SerializeField] float dashForce = 8f;
-    [SerializeField] private bool canJump = true;
-    private bool canDash = true;
-    [SerializeField] private bool canDoubleJump = true;
-    
-    [Header("Drag")]
-    float groundDrag = 6f;
-    float airDrag = 0.8f;
+	[Header("Movement")]
+	private float moveSpeed;
+	public float walkSpeed = 6f;
+	public float sprintSpeed = 12f;
+	public float movementMultiplier = 10f;
+	public float playerHeight = 2;
+	[SerializeField] float acceleration;
+	[SerializeField] float airMultiplier = 0.8f;
+	[SerializeField] float jumpCooldown = 0.2f;
+	[SerializeField] float dashCooldown = 2f;
+	[SerializeField] Transform orientation;
+	[SerializeField] float jumpForce = 50f;
+	[SerializeField] float dashForce = 8f;
+	[SerializeField] private bool canJump = true;
+	private bool canDash = true;
+	[SerializeField] private bool canDoubleJump = true;
 
-    float horizontalMovement;
-    float verticalMovement;
+	[Header("Drag")]
+	float groundDrag = 6f;
+	float airDrag = 0.8f;
 
-    [Header("Keybinds")]
-    [SerializeField] KeyCode jumpKey = KeyCode.Space;
-    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
-    [SerializeField] KeyCode dashkey = KeyCode.V;
+	float horizontalMovement;
+	float verticalMovement;
 
-    [Header("Ground Detection")]
-    [SerializeField] Transform groundCheck;
-    [SerializeField] float groundDistance = 0.2f;
-    [SerializeField] LayerMask whatIsGround;
-    public bool isGrounded;
+	[Header("Keybinds")]
+	[SerializeField] KeyCode jumpKey = KeyCode.Space;
+	[SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
+	[SerializeField] KeyCode dashkey = KeyCode.V;
 
-    Vector3 moveDirection;
-    Vector3 slopeMoveDirection;
+	[Header("Ground Detection")]
+	[SerializeField] Transform groundCheck;
+	[SerializeField] float groundDistance = 0.2f;
+	[SerializeField] LayerMask whatIsGround;
+	public bool isGrounded;
 
-    Rigidbody rb;
-    WallRunV2 wr;
-    RaycastHit slopeHit;
+	Vector3 moveDirection;
+	Vector3 slopeMoveDirection;
 
-    private void Start()
-    {
-        wr = GetComponent<WallRunV2>();
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
-    }
+	Rigidbody rb;
+	WallRunV2 wr;
+	RaycastHit slopeHit;
 
-    private void Update()
-    {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround);
+	private void Start()
+	{
+		wr = GetComponent<WallRunV2>();
+		rb = GetComponent<Rigidbody>();
+		rb.freezeRotation = true;
+	}
 
-        MyInput();
-        ControlDrag();
+	private void Update()
+	{
+		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround);
 
-        if (isGrounded)
-        {
-            canDoubleJump = true;
-        }
+		MyInput();
+		ControlDrag();
 
-        slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
-    }
+		if (isGrounded) canDoubleJump = true;
 
-    void MyInput()
-    {
-        horizontalMovement = Input.GetAxisRaw("Horizontal");
-        verticalMovement = Input.GetAxisRaw("Vertical");
+		slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+	}
 
-        moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
+	void MyInput()
+	{
+		horizontalMovement = Input.GetAxisRaw("Horizontal");
+		verticalMovement = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(jumpKey) && canJump && isGrounded)
-        {
-            canJump = false;
+		moveDirection = (orientation.forward * verticalMovement + orientation.right * horizontalMovement).normalized;
 
-            Jump();
+		if (Input.GetKey(jumpKey) && canJump && isGrounded)
+		{
+			canJump = false;
 
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
-        
-        if (Input.GetKeyDown(jumpKey) && !isGrounded && canDoubleJump && !wr.isWallrunning)
-        {
-            canDoubleJump = false;
+			Jump();
 
-            Jump();
-        }
+			Invoke(nameof(ResetJump), jumpCooldown);
+		}
 
-        if (Input.GetKeyDown(dashkey) && canDash && isGrounded)
-        {
-            canDash = false;
+		if (Input.GetKeyDown(jumpKey) && !isGrounded && canDoubleJump && !wr.isWallrunning)
+		{
+			canDoubleJump = false;
 
-            Dash();
+			Jump();
+		}
 
-            Invoke(nameof(ResetDash), dashCooldown);
-        }
+		if (Input.GetKeyDown(dashkey) && canDash && isGrounded)
+		{
+			canDash = false;
 
-        if (Input.GetKey(sprintKey) && isGrounded)
-        {
-            moveSpeed = sprintSpeed;
-        }
-        else
-        {
-            moveSpeed = walkSpeed;
-        }
-    }
+			Dash();
 
-    private void FixedUpdate()
-    {
-        MovePlayer();
-    }
+			Invoke(nameof(ResetDash), dashCooldown);
+		}
 
-    void MovePlayer()
-    {
-        if (isGrounded && !OnSlope())
-        {
-            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
-        }
-        else if (isGrounded && OnSlope())
-        {
-            rb.AddForce(slopeMoveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
-        }
-        else if (!isGrounded) 
-        {
-            rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Acceleration);
-        }
-    }
+		if (Input.GetKey(sprintKey) && isGrounded)
+		{
+			moveSpeed = sprintSpeed;
+		}
+		else
+		{
+			moveSpeed = walkSpeed;
+		}
+	}
 
-    void Jump()
-    {
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-    }
+	private void FixedUpdate()
+	{
+		MovePlayer();
+	}
 
-    void ResetJump()
-    {
-        canJump = true;
-    }
+	void MovePlayer()
+	{
+		if (isGrounded)
+		{
+			rb.AddForce(movementMultiplier * moveSpeed * (OnSlope() ? slopeMoveDirection : moveDirection), ForceMode.Acceleration);
+		}
+		else
+		{
+			rb.AddForce(airMultiplier * moveSpeed * moveDirection, ForceMode.Acceleration);
+		}
+	}
 
-    void Dash()
-    {
-        rb.AddForce(moveDirection.normalized * dashForce, ForceMode.Impulse);
-    }
+	void Jump()
+	{
+		rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+		rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+	}
 
-    void ResetDash()
-    {
-        canDash = true;
-    }
+	void ResetJump()
+	{
+		canJump = true;
+	}
 
-    void Crouch()
-    {
-        // Crouch
-    }
+	void Dash()
+	{
+		rb.AddForce(moveDirection * dashForce, ForceMode.Impulse);
+	}
 
-    void ControlDrag()
-    {
-        if (isGrounded)
-        {
-            rb.drag = groundDrag;
-        }
-        else if (wr.isWallrunning)
-        {
-            rb.drag = groundDrag;
-        }
-        else
-        {
-            rb.drag = airDrag;
-        }
-    }
+	void ResetDash()
+	{
+		canDash = true;
+	}
 
-    void ControlSpeed()
-    {
-        if (Input.GetKey(sprintKey) && isGrounded)
-        {
-            moveSpeed = Mathf.Lerp(moveSpeed, sprintSpeed, acceleration * Time.deltaTime);
-        }
-        else
-        {
-            moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, acceleration * Time.deltaTime);
-        }
-    }
+	void Crouch()
+	{
+		// Crouch
+	}
 
-    private bool OnSlope()
-    {
-        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
-        {
-            if (slopeHit.normal != Vector3.up)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        return false;
-    }
+	void ControlDrag()
+	{
+		if (isGrounded)
+		{
+			rb.drag = groundDrag;
+		}
+		else if (wr.isWallrunning)
+		{
+			rb.drag = groundDrag;
+		}
+		else
+		{
+			rb.drag = airDrag;
+		}
+	}
+
+	void ControlSpeed()
+	{
+		if (Input.GetKey(sprintKey) && isGrounded)
+		{
+			moveSpeed = Mathf.Lerp(moveSpeed, sprintSpeed, acceleration * Time.deltaTime);
+		}
+		else
+		{
+			moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, acceleration * Time.deltaTime);
+		}
+	}
+
+	private bool OnSlope()
+	{
+		if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
+		{
+			if (slopeHit.normal != Vector3.up)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return false;
+	}
 }
