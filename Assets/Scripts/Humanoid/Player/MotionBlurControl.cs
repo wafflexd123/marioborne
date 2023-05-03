cabfd85ea5bbe9618b9924ef5c982b445e17e183
Thomas Zhu <silver6219@hotmail.com>
2023-05-03 14:29:04 +1000

    #75 and #82 Added a new font for subtitles and a white vignette effect for running.
    Vignette effect increases in intensity when speed increases.

---
+++ Assets/Scripts/Humanoid/Player/MotionBlurControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MotionBlurControl : MonoBehaviour
+{
+    public Image edgeBlurImage;
+    public float minSpeedForEffect = 5f;
+    public float maxSpeedForEffect = 10f;
+    public float minAlpha = 0f;
+    public float maxAlpha = 0.5f;
+
+    private GameObject player;
+    private Rigidbody playerRigidbody;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player)
+        {
+            playerRigidbody = player.GetComponent<Rigidbody>();
+        }
+    }
+
+    void Update()
+    {
+        if (playerRigidbody)
+        {
+            float playerSpeed = playerRigidbody.velocity.magnitude;
+            float alpha = Mathf.Lerp(minAlpha, maxAlpha, (playerSpeed - minSpeedForEffect) / (maxSpeedForEffect - minSpeedForEffect));
+            alpha = Mathf.Clamp(alpha, minAlpha, maxAlpha);
+
+            Color newColor = edgeBlurImage.color;
+            newColor.a = alpha;
+            edgeBlurImage.color = newColor;
+        }
+    }
+}
