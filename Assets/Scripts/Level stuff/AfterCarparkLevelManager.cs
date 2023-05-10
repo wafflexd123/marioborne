faf01d102d9cc452146a7f95b7adb58fd4c3c7c2
wafflexd123 <54018924+wafflexd123@users.noreply.github.com>
2023-05-11 08:36:25 +1000

    #64 after carpark level mostly complete

--- Assets/Scripts/Level stuff/AfterCarparkLevelManager.cs
+++ Assets/Scripts/Level stuff/AfterCarparkLevelManager.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AfterCarparkLevelManager : MonoBehaviour
 {
-    public TriggerCollider firstEnemyTrigger;
-    public EnemyPath[] enemySpawns;
+	public TriggerCollider firstEnemyTrigger;
+	public EnemyPath[] enemySpawns;
+	public ElevatorDoor elevator;
+	public TriggerCollider elevatorTrigger;
+	public ElevatorButton elevatorButton;
+	public float sceneLoadDelay;
 
-    IEnumerator Start()
-    {
-        yield return new WaitUntil(() => firstEnemyTrigger.isTriggered);
-        enemySpawns[0].SetPaths();
-    }
+	IEnumerator Start()
+	{
+		Debug.Log("Stage 0");
+		yield return new WaitUntil(() => firstEnemyTrigger.isTriggered);
+		Debug.Log("Stage 1");
+		enemySpawns[0].SetPaths();
+		yield return new WaitUntil(() => elevator.IsFullyClosed && elevatorTrigger.isTriggered);//elevator is closed and player is inside elevator
+		Debug.Log("Stage 2");
+		elevatorButton.interactable = false;
+		yield return new WaitForSeconds(sceneLoadDelay);
+		Debug.Log("Stage 3");
+		SceneManager.LoadSceneAsync("Level Select", LoadSceneMode.Single);
+	}
 }
