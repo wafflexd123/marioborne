8a15586e03a8a3338ef0f20df566eb828337bda6
Thomas Zhu <silver6219@hotmail.com>
2023-05-10 20:53:09 +1000

    #99 Added level select, currently slightly bugged

---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public GameObject[] levelPanels;
+    public Button[] leftArrowButtons;
+    public Button[] rightArrowButtons;
+
+    private int currentPanelIndex = 0;
+
+    private void Start()
+    {
+
+        for (int i = 0; i < leftArrowButtons.Length; i++)
+        {
+            leftArrowButtons[i].onClick.AddListener(OnLeftArrowClick);
+        }
+
+        for (int i = 0; i < rightArrowButtons.Length; i++)
+        {
+            rightArrowButtons[i].onClick.AddListener(OnRightArrowClick);
+        }
+    }
+
+    public void LoadLevel(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void OnLeftArrowClick()
+    {
+        if (currentPanelIndex > 0)
+        {
+            currentPanelIndex--;
+            UpdatePanels();
+        }
+    }
+
+    public void OnRightArrowClick()
+    {
+        if (currentPanelIndex < levelPanels.Length - 1)
+        {
+            currentPanelIndex++;
+            UpdatePanels();
+        }
+    }
+
+    private void UpdatePanels()
+    {
+        for (int i = 0; i < levelPanels.Length; i++)
+        {
+            levelPanels[i].SetActive(i == currentPanelIndex);
+        }
+    }
+}
