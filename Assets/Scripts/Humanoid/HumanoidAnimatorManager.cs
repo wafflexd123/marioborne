5478cc6140fe288c9f88416dda92aff44db7fedf
wafflexd123 <54018924+wafflexd123@users.noreply.github.com>
2023-05-24 23:44:56 +1000

    #106 deflecting/punching use animation events instead of hard-coded timings

--- Assets/Scripts/Humanoid/HumanoidAnimatorManager.cs
+++ Assets/Scripts/Humanoid/HumanoidAnimatorManager.cs
@@ -5,45 +5,45 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class HumanoidAnimatorManager : MonoBehaviourPlus
 {
-    // Add your footstep sounds here
-    public AudioClip[] footstepSounds;
-    public AudioClip[] jumpingSounds;
-    public AudioClip[] landingSounds;
+	// Add your footstep sounds here
+	public AudioClip[] footstepSounds;
+	public AudioClip[] jumpingSounds;
+	public AudioClip[] landingSounds;
 
-    public float walkSpeed, runSpeed, colliderCrouchTime, crouchHeightMultiplier;
+	public float walkSpeed, runSpeed, colliderCrouchTime, crouchHeightMultiplier, minStepVolume, maxStepVolume, velocityAtMaxStepVolume;
 
-    private AudioSource audioSource;
-    private float colliderHeight, colliderHeightCrouch, colliderCentreCrouch;
-    private bool _wallRunning, _grounded;
-    private Vector3 colliderCentre;
-    private Animator animator;
-    private Coroutine crtGroundState, crtCrouch;
-    new private CapsuleCollider collider;
+	private AudioSource audioSource;
+	private float colliderHeight, colliderHeightCrouch, colliderCentreCrouch;
+	private bool _wallRunning, _grounded, _punching, _deflect;
+	private Vector3 colliderCentre;
+	private Animator animator;
+	private Coroutine crtGroundState, crtCrouch;
+	new private CapsuleCollider collider;
 	private Vector3 _velocity;
 
-    private void Awake()
-    {
-        audioSource = GetComponent<AudioSource>();
-        animator = GetComponent<Animator>();
-        collider = transform.parent.GetComponent<CapsuleCollider>();
-        colliderHeight = collider.height;
-        colliderHeightCrouch = colliderHeight * crouchHeightMultiplier;
-        colliderCentre = collider.center;
-        colliderCentreCrouch = colliderCentre.y * crouchHeightMultiplier;
+	private void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+		animator = GetComponent<Animator>();
+		collider = transform.parent.GetComponent<CapsuleCollider>();
+		colliderHeight = collider.height;
+		colliderHeightCrouch = colliderHeight * crouchHeightMultiplier;
+		colliderCentre = collider.center;
+		colliderCentreCrouch = colliderCentre.y * crouchHeightMultiplier;
 
-        if (transform.localPosition != Vector3.zero)
-        {
-            transform.localPosition = Vector3.zero;
-            Debug.LogWarning("The model position of " + name + " is not zero. Resetting.");
-        }
-        if (transform.localEulerAngles != Vector3.zero)
-        {
-            transform.localEulerAngles = Vector3.zero;
-            Debug.LogWarning("The model rotation of " + name + " is not zero. Resetting.");
-        }
-    }
+		if (transform.localPosition != Vector3.zero)
+		{
+			transform.localPosition = Vector3.zero;
+			Debug.LogWarning("The model position of " + name + " is not zero. Resetting.");
+		}
+		if (transform.localEulerAngles != Vector3.zero)
+		{
+			transform.localEulerAngles = Vector3.zero;
+			Debug.LogWarning("The model rotation of " + name + " is not zero. Resetting.");
+		}
+	}
 
-    void CheckGroundState()
+	void CheckGroundState()
 	{
 		if (crtGroundState == null) crtGroundState = StartCoroutine(Routine());
 		IEnumerator Routine()
@@ -83,11 +83,11 @@ public class HumanoidAnimatorManager : MonoBehaviourPlus
 	public bool roll { set => animator.SetTrigger("roll"); }
 	public bool land { set => animator.SetTrigger("land"); }
 	public bool holdingMelee { set => animator.SetLayerWeight(1, value ? 1 : 0); }
-    public bool holdingGun { set => animator.SetLayerWeight(2, value ? 1 : 0); }
-    public bool punching { set => animator.SetBool("punching", value); }
-    public bool melee { set => animator.SetBool("melee", value); }
-    public bool shooting { set => animator.SetBool("shooting", value); }
-    public bool deflect { set => animator.SetBool("deflect", value); }
+	public bool holdingGun { set => animator.SetLayerWeight(2, value ? 1 : 0); }
+	public bool punching { set { if (value) { animator.SetTrigger("punching"); _punching = true; } } get => _punching; }
+	public bool melee { set => animator.SetBool("melee", value); }
+	public bool shooting { set => animator.SetBool("shooting", value); }
+	public bool deflect { set { if (value) { animator.SetTrigger("deflect"); _deflect = true; } } get => _deflect; }
 	public bool dying { set => animator.SetBool("dying", value); }
 	public bool grounded { set { _grounded = value; CheckGroundState(); } }
 	public bool wallRunning { set { _wallRunning = value; CheckGroundState(); } }
@@ -107,32 +107,42 @@ public class HumanoidAnimatorManager : MonoBehaviourPlus
 		}
 	}
 
-    public void PlayFootstepSound()
-    {
-        if (footstepSounds.Length == 0) return; 
+	public void EndPunch()
+	{
+		_punching = false;
+	}
+
+	public void EndDeflect()
+	{
+		_deflect = false;
+	}
+
+	public void PlayFootstepSound()
+	{
+		if (footstepSounds.Length == 0) return;
 		// Pick random sound from array
-        AudioClip footstepSound = footstepSounds[Random.Range(0, footstepSounds.Length)];
+		AudioClip footstepSound = footstepSounds[Random.Range(0, footstepSounds.Length)];
 
-        // Change the volume based on the player's velocity
-        float volume = Mathf.Sqrt((_velocity.x * _velocity.x) + (_velocity.z * _velocity.z)) / 10.0f; // Adjust if you want (Probably necessary but I dunno)
-        volume = Mathf.Clamp(volume, 0.1f, 1.0f);
+		// Change the volume based on the player's velocity
+		float volume = Mathf.Lerp(minStepVolume, maxStepVolume, Mathf.Sqrt((_velocity.x * _velocity.x) + (_velocity.z * _velocity.z)) / velocityAtMaxStepVolume);
+		volume = Mathf.Clamp(volume, 0.1f, 1.0f);
 
-        audioSource.PlayOneShot(footstepSound, volume);
-    }
+		audioSource.PlayOneShot(footstepSound, volume);
+	}
 
 	public void PlayJumpSound()
 	{
-        if (jumpingSounds.Length == 0) return;
-        AudioClip jumpSound = jumpingSounds[Random.Range(0, jumpingSounds.Length)];
+		if (jumpingSounds.Length == 0) return;
+		AudioClip jumpSound = jumpingSounds[Random.Range(0, jumpingSounds.Length)];
 
-        audioSource.PlayOneShot(jumpSound);
-    }
+		audioSource.PlayOneShot(jumpSound);
+	}
 
 	public void PlayLandingSound()
 	{
-        if (landingSounds.Length == 0) return;
+		if (landingSounds.Length == 0) return;
 		AudioClip landingSound = landingSounds[Random.Range(0, landingSounds.Length)];
 
-        audioSource.PlayOneShot(landingSound);
-    }
+		audioSource.PlayOneShot(landingSound);
+	}
 }
