74e619cb66d5523dc658f5f6a26e7bbac3d9fd79
wafflexd123 <54018924+wafflexd123@users.noreply.github.com>
2023-05-28 17:43:57 +1000

    #111 fixed checkpoints>????!??!

--- Assets/Scripts/Utilities/CheckpointManager.cs
+++ Assets/Scripts/Utilities/CheckpointManager.cs
@@ -8,24 +8,25 @@ public class CheckpointManager : MonoBehaviour
 	public static CheckpointManager instance;
 	public Vector3 lastCheckpointPos;
 	public int lastCheckpoint;
-	int lastScene;
+	int lastScene = -1;
 
 	void Awake()
 	{
 		if (instance == null)
 		{
 			instance = this;
-			lastScene = SceneManager.GetActiveScene().buildIndex;
 			DontDestroyOnLoad(gameObject);
 			SceneManager.activeSceneChanged += (Scene a, Scene b) =>
 			{
 				if (lastScene == b.buildIndex)
 				{
 					Player.singlePlayer.transform.position = lastCheckpointPos;//if reloading the same scene, spawn at checkpoint
+					StartCoroutine(IHateSocietyAndUnityAndEverythingSucks(lastCheckpointPos));
 				}
 				else
 				{
 					lastCheckpoint = 0;
+					lastCheckpointPos = Player.singlePlayer.transform.position;//player will respawn where it starts in the level, until hitting a checkpoint
 					lastScene = b.buildIndex;//if reloading to a different scene, store new scene
 				}
 			};
@@ -34,6 +35,12 @@ public class CheckpointManager : MonoBehaviour
 		{
 			Destroy(gameObject);
 		}
+	}
+
+	IEnumerator IHateSocietyAndUnityAndEverythingSucks(Vector3 pos)
+	{
+		yield return new WaitForFixedUpdate();
+		Player.singlePlayer.transform.position = pos;//bruh
 	}
 }
 
