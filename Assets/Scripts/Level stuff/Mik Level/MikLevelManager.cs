325eaf78fc6d9473660addec19dbfa6201c2f6b6
Miksi64 <53842268+Miksi64@users.noreply.github.com>
2023-05-28 19:57:13 +1000

    #111 (hopefully) optimized Mik level a little bit
    marked objects for deletion on reload

--- Assets/Scripts/Level stuff/Mik Level/MikLevelManager.cs
+++ Assets/Scripts/Level stuff/Mik Level/MikLevelManager.cs
@@ -5,10 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MikLevelManager : MonoBehaviour
 {
+
+    public GameObject Enemies1, Enemies2, Enemies3, Building2, Building25, Building3, Building4, Building5, BgBuildings;
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch (CheckpointManager.instance.lastCheckpoint)
+        {
+            case 1:
+                Destroy(Enemies1);
+                Destroy(Enemies2);
+                Destroy(Building2);
+                break;
+
+            case 2:
+                Destroy(Building2);
+                Destroy(Building25);
+                Destroy(Building3);
+                Destroy(Building4);
+                Destroy(Building5);
+                break;
+        }
     }
 
     // Update is called once per frame
