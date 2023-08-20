aab09424d428da7c23811acc02514749491d91cb
wafflexd123 <54018924+wafflexd123@users.noreply.github.com>
2023-08-20 17:41:32 +1000

    added cool font

--- Assets/Scripts/Level Specific/Dream Level/DreamLevelManager.cs
+++ Assets/Scripts/Level Specific/Dream Level/DreamLevelManager.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class DreamLevelManager : MonoBehaviourPlus
 {
 	public RawImage whiteFade;
 	public float startTime, whiteFadeTime;
-	public GameObject firstSection;
+	public string nextScene;
+	public GameObject player;
+	public bool switchScene;
+
 	IEnumerator Start()
 	{
-		float timer;
 		whiteFade.gameObject.SetActive(true);
-		for (timer = 0; timer < startTime; timer += Time.deltaTime) yield return null;
-		for (timer = 0; timer < whiteFadeTime; timer += Time.deltaTime)
+		yield return new WaitForSeconds(startTime);
+		for (float timer = 0; timer < whiteFadeTime; timer += Time.deltaTime)
 		{
 			whiteFade.color = new Color(1, 1, 1, 1 - (timer / whiteFadeTime));
 			yield return null;
 		}
 		Destroy(whiteFade.gameObject);
-		yield return new WaitUntil(() => firstSection == null);
 	}
 }
