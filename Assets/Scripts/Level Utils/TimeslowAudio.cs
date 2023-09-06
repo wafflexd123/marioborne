11d7cefa16b0cbf891b0e70167e078e7854145b3
wafflexd123 <54018924+wafflexd123@users.noreply.github.com>
2023-09-06 22:23:49 +1000

    audio reverse & player isnt jittery while reversing

--- Assets/Scripts/Level Utils/TimeslowAudio.cs
+++ Assets/Scripts/Level Utils/TimeslowAudio.cs
@@ -1,25 +1,44 @@
 using UnityEngine;
 
-public class TimeslowAudio : MonoBehaviour, ITimeScaleListener
+public class TimeslowAudio : MonoBehaviour, ITimeScaleListener, IRewindListener
 {
     AudioSource audioSource;
     float startPitch;
     public float minPitch = .7f;
+    int pitchDirection = 1;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
         startPitch = audioSource.pitch;
         Time.timeScaleListeners.Add(this);
+        Time.rewindListeners.Add(this);
     }
 
     public void OnTimeSlow()
     {
-        audioSource.pitch = Mathf.Lerp(minPitch, startPitch, Mathf.InverseLerp(Time.minTimeScale, 1, Time.timeScale));
+        audioSource.pitch = Mathf.Lerp(minPitch, startPitch, Mathf.InverseLerp(Time.minTimeScale, 1, Time.timeScale)) * pitchDirection;
     }
 
     private void OnDestroy()
     {
         Time.timeScaleListeners.Remove(this);
+        Time.rewindListeners.Remove(this);
+    }
+
+    public void Rewind(float seconds)
+	{
+        OnTimeSlow();
+    }
+
+	public void StartRewind()
+	{
+        pitchDirection = -1;
+    }
+
+    public void StopRewind()
+	{
+        pitchDirection = 1;
+        OnTimeSlow();
     }
 }
