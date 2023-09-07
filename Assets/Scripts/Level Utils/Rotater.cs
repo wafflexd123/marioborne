ce8ddacad334a155455790cc358f399cb0217284
Thomas Zhu <silver6219@hotmail.com>
2023-09-07 23:58:09 +1000

    Parkour level layout and function finished
    Made a change to Rotater scrip to allow it to rotate directly to a specified rotation

--- Assets/Scripts/Level Utils/Rotater.cs
+++ Assets/Scripts/Level Utils/Rotater.cs
@@ -3,41 +3,43 @@ using UnityEngine;
 
 public class Rotater : MonoBehaviour
 {
-	public EasingFunction.Enum easingFunction;
-	public Vector3 rotateAmount;
-	[field: SerializeField] public float TimeToRotate { get; set; }
-	[field: SerializeField] public bool RotateOnEnable { get; set; }
-	[field: SerializeField] public bool Loop { get; set; }
+    public EasingFunction.Enum easingFunction;
+    public Vector3 rotateAmount;
+    [field: SerializeField] public float TimeToRotate { get; set; }
+    [field: SerializeField] public bool RotateOnEnable { get; set; }
+    [field: SerializeField] public bool Loop { get; set; }
+    [field: SerializeField] public bool RotateToSpecificRotation { get; set; } = false; // New field
+    [field: SerializeField] public Vector3 SpecificRotation { get; set; } // New field
 
-	public void StartRotation()
-	{
-		Vector3 start = transform.eulerAngles;
-		Vector3 end = rotateAmount + start;
-		float timer, percent;
-		StartCoroutine(Rotate());
-		IEnumerator Rotate()
-		{
-			do
-			{
-				timer = 0;
-				do
-				{
-					timer += Time.fixedDeltaTime;
-					percent = timer / TimeToRotate;
-					transform.eulerAngles = Vector3.Lerp(start, end, EasingFunction.Get(easingFunction)(percent));
-					yield return new WaitForFixedUpdate();
-				} while (percent < 1);
-			} while (Loop);
-		}
-	}
+    public void StartRotation()
+    {
+        Vector3 start = transform.eulerAngles;
+        Vector3 end = RotateToSpecificRotation ? SpecificRotation : rotateAmount + start; // Modified line
+        float timer, percent;
+        StartCoroutine(Rotate());
+        IEnumerator Rotate()
+        {
+            do
+            {
+                timer = 0;
+                do
+                {
+                    timer += Time.fixedDeltaTime;
+                    percent = timer / TimeToRotate;
+                    transform.eulerAngles = Vector3.Lerp(start, end, EasingFunction.Get(easingFunction)(percent));
+                    yield return new WaitForFixedUpdate();
+                } while (percent < 1);
+            } while (Loop);
+        }
+    }
 
-	private void OnEnable()
-	{
-		if (RotateOnEnable) StartRotation();
-	}
+    private void OnEnable()
+    {
+        if (RotateOnEnable) StartRotation();
+    }
 
-	private void OnDisable()
-	{
-		StopAllCoroutines();
-	}
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
 }
