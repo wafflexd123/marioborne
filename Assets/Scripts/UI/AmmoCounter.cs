6f24193bb52ecf28519f011b265f9e209d6306f5
wafflexd123 <54018924+wafflexd123@users.noreply.github.com>
2023-10-30 05:09:57 +1100

    ammo counter says Q when out of ammo

--- Assets/Scripts/UI/AmmoCounter.cs
+++ Assets/Scripts/UI/AmmoCounter.cs
@@ -4,44 +4,44 @@ using UnityEngine;
 
 public class AmmoCounter : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI currentAmmoText;
-    [SerializeField] private TextMeshProUGUI nextAmmoText;
+	[SerializeField] private TextMeshProUGUI currentAmmoText;
+	[SerializeField] private TextMeshProUGUI nextAmmoText;
 
-    public void SetAmmo(int newAmmo)
-    {
-        nextAmmoText.text = newAmmo.ToString();
-        StartCoroutine(ChangeAmmoEffect());
-    }
+	public void SetAmmo(int newAmmo)
+	{
+		nextAmmoText.text = newAmmo > 0 ? $"{newAmmo}" : "Q";
+		StartCoroutine(ChangeAmmoEffect());
+	}
 
-    private IEnumerator ChangeAmmoEffect()
-    {
-        float moveAmount = 0.0468f;
+	private IEnumerator ChangeAmmoEffect()
+	{
+		float moveAmount = 0.0468f;
 
-        Vector3 currentStartPos = new Vector3(0, 0, 0);
-        Vector3 nextStartPos = new Vector3(0, moveAmount, 0);
+		Vector3 currentStartPos = new Vector3(0, 0, 0);
+		Vector3 nextStartPos = new Vector3(0, moveAmount, 0);
 
-        currentAmmoText.rectTransform.localPosition = currentStartPos;
-        nextAmmoText.rectTransform.localPosition = nextStartPos;
+		currentAmmoText.rectTransform.localPosition = currentStartPos;
+		nextAmmoText.rectTransform.localPosition = nextStartPos;
 
-        Vector3 currentEndPos = currentStartPos - moveAmount * Vector3.up;
-        Vector3 nextEndPos = nextStartPos - moveAmount * Vector3.up;
+		Vector3 currentEndPos = currentStartPos - moveAmount * Vector3.up;
+		Vector3 nextEndPos = nextStartPos - moveAmount * Vector3.up;
 
-        float duration = 0.1f;
-        float elapsed = 0f;
+		float duration = 0.1f;
+		float elapsed = 0f;
 
-        while (elapsed < duration)
-        {
-            elapsed += Time.deltaTime;
-            float t = elapsed / duration;
+		while (elapsed < duration)
+		{
+			elapsed += Time.deltaTime;
+			float t = elapsed / duration;
 
-            currentAmmoText.rectTransform.localPosition = Vector3.Lerp(currentStartPos, currentEndPos, t);
-            nextAmmoText.rectTransform.localPosition = Vector3.Lerp(nextStartPos, nextEndPos, t);
+			currentAmmoText.rectTransform.localPosition = Vector3.Lerp(currentStartPos, currentEndPos, t);
+			nextAmmoText.rectTransform.localPosition = Vector3.Lerp(nextStartPos, nextEndPos, t);
 
-            yield return null;
-        }
+			yield return null;
+		}
 
-        currentAmmoText.text = nextAmmoText.text;
-        currentAmmoText.rectTransform.localPosition = currentStartPos;
-        nextAmmoText.rectTransform.localPosition = nextStartPos;
-    }
+		currentAmmoText.text = nextAmmoText.text;
+		currentAmmoText.rectTransform.localPosition = currentStartPos;
+		nextAmmoText.rectTransform.localPosition = nextStartPos;
+	}
 }
